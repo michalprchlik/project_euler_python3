40e0da2c553c509523a6032bd6867d111652742f
michal <michal@localhost>
2021-09-14 15:36:54 +0200

    add even_fibonacci_numbers solution

---
+++ even_fibonacci_numbers/application.py
@@ -0,0 +1,5 @@
+from function import *
+
+list_of_numbers = get_numbers(4000000)
+result = get_sum_of_numbers(list_of_numbers)
+print (result)
--- even_fibonacci_numbers/test.py
+++ even_fibonacci_numbers/test.py
@@ -7,57 +7,62 @@ class TestFunction(unittest.TestCase):
 
 	def test_get_numbers(self):
 		max_number=1
-		expected_result=[1]
-		result=get_numbers(max_number)
-		self.assertEqual(expected_result, result, 'max_number = 1')
-		
-		max_number=0
 		expected_result=[]
 		result=get_numbers(max_number)
-		self.assertEqual(expected_result, result, 'max_number = 0')
+		self.assertEqual(expected_result, result, 'max_number = ' + str(max_number))
+		
+		max_number=2
+		expected_result=[1]
+		result=get_numbers(max_number)
+		self.assertEqual(expected_result, result, 'max_number = ' + str(max_number))
 		
 		max_number=10
-		expected_result=[0, 3, 5, 6, 9]
+		expected_result=[1, 2, 3, 5, 8]
 		result=get_numbers(max_number)
-		self.assertEqual(expected_result, result, 'max_number = 10')
+		self.assertEqual(expected_result, result, 'max_number = ' + str(max_number))
 		
 		max_number=20
-		expected_result=[0, 3, 5, 6, 9, 10, 12, 15, 18]
+		expected_result=[1, 2, 3, 5, 8, 13]
 		result=get_numbers(max_number)
-		self.assertEqual(expected_result, result, 'max_number = 20')
-
+		self.assertEqual(expected_result, result, 'max_number = ' + str(max_number))
+		
+		max_number=100
+		expected_result=[1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+		result=get_numbers(max_number)
+		self.assertEqual(expected_result, result, 'max_number = ' + str(max_number))
+		
 	def test_get_sum_of_numbers(self):
-		list_of_numbers = []
-		expected_result=0
+		list_of_numbers = [1, 2, 3, 5, 8]
+		expected_result=10
 		result=get_sum_of_numbers(list_of_numbers)
-		self.assertEqual(expected_result, result, '[]')
+		self.assertEqual(expected_result, result, '[1, 2, 3, 5, 8]')
 
-		list_of_numbers = [0, 3, 5, 6, 9]
-		expected_result=23
+		list_of_numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+		expected_result=44
 		result=get_sum_of_numbers(list_of_numbers)
-		self.assertEqual(expected_result, result, '[0, 3, 5, 6, 9]')
+		self.assertEqual(expected_result, result, '[1, 2, 3, 5, 8, 13, 21, 34, 55, 89]')
 
 	def test_integration(self):
 		max_number=10
-		expected_result=23
+		expected_result=10
 		list_of_numbers=get_numbers(max_number)
 		result=get_sum_of_numbers(list_of_numbers)
 		self.assertEqual(expected_result, result, 'max_number=10')
 
 		max_number=100
-		expected_result=2318
+		expected_result=44
 		list_of_numbers=get_numbers(max_number)
 		result=get_sum_of_numbers(list_of_numbers)
 		self.assertEqual(expected_result, result, 'max_number=100')
 
 		max_number=1000
-		expected_result=233168
+		expected_result=798
 		list_of_numbers=get_numbers(max_number)
 		result=get_sum_of_numbers(list_of_numbers)
 		self.assertEqual(expected_result, result, 'max_number=1000')
 
 		max_number=10000
-		expected_result=23331668
+		expected_result=3382
 		list_of_numbers=get_numbers(max_number)
 		result=get_sum_of_numbers(list_of_numbers)
 		self.assertEqual(expected_result, result, 'max_number=10000')
